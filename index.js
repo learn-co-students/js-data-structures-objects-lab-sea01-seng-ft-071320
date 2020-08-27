d3c8663a0ec7ea212d2e9c62259831afc8483bcb
dbpatnodee <dbpatnode@hotmail.com>
2020-08-26 21:26:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,70 @@
-const driver = {};
+// const driver = {};
 
-function updateDriverWithKeyAndValue(driver, key, value){
-    return Object.assign({}, driver, {[key]:value});
+// function updateDriverWithKeyAndValue(driver, key, value){
+//     return Object.assign({}, driver, {[key]:value});
+// }
+
+// function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+//     driver[key] = value;
+    
+//     return driver;
+// } 
+
+// function deleteFromDriverByKey(driver, key) {
+
+//     const x = Object.assign({}, driver);
+
+//     delete x[key];
+
+//     return x;
+// }
+
+// function destructivelyDeleteFromDriverByKey(driver, key) {
+//     delete driver[key];
+//     return driver;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, {[key]:value})
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-    driver[key] = value;
-    
-    return driver;
-} 
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign(driver, {[key]:value})
+}
 
-function deleteFromDriverByKey(driver, key) {
-
-    const x = Object.assign({}, driver);
-
-    delete x[key];
-  
-    return x;
+function deleteFromDriverByKey(driver,key) {
+    const newDriver = Object.assign({}, driver, [key])
+    delete newDriver[key]
+    return newDriver
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-    delete driver[key];
-    return driver;
+    delete driver[key]
+    return driver
 }
